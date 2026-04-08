80a817b4cfbe2c9104d45de31b37caab988c11b1
Lucas Castelano <lucas.castelano27@gmail.com>
2026-04-08 19:46:55 -0300

    add api-js cicd

---
+++ APIs para exercícios/api-js/sonar-project.js
@@ -0,0 +1,17 @@
+import scanner from "sonarqube-scanner"
+
+scanner(
+  {
+    serverUrl: process.env.SONAR_HOST_URL || "http://localhost:9000",
+    token: process.env.SONAR_TOKEN || "",
+    options: {
+      "sonar.projectKey": process.env.SONAR_PROJECT_KEY || "api-js",
+      "sonar.projectName": "API JS",
+      "sonar.sources": ".",
+      "sonar.exclusions":
+        "node_modules/**,coverage/**,tests/**,sonar-project.js",
+      "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
+    },
+  },
+  () => process.exit(),
+)
